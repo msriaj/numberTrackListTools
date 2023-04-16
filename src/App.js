6ba4e977d94f3e9c0a2c9ca5f17c711df7da7196
msriaj <contact.riaj@gmail.com>
2023-04-16 22:18:28 +0600

    asd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import { TelegramShareButton } from 'react-share';
+
 import './App.css';
 import React, { useState } from 'react';
 
@@ -37,36 +38,53 @@ function App() {
         <textarea value={data.map(item => `${item.amount} ${item.phoneNumber}`).join('\n')} onChange={handleDataChange}></textarea>
       </div>
 
-      <div className='singleData heading'>
-        <label>
-          <input className='amount' type="text" name='amount' value={"Amount"} readOnly />
-        </label>
-        <label>
-          <input className='phoneNumber' type="text" name='phoneNumber' value={"Phone Number"} readOnly />
-        </label>
-        <label>
-          <input className='transactions' type="text" name='transactionId' value={"Transaction Id"} readOnly />
-        </label>
-      </div>
-      {data.map((item, index) => (
-        <div key={index} className='singleData'>
-          <label>
-            <input className='amount' type="text" name='amount' onClick={handleCopyToClipboard} value={item.amount} readOnly />
-          </label>
-          <label>
-            <input className='phoneNumber' type="text" name='phoneNumber' value={item.phoneNumber} onClick={handleCopyToClipboard} readOnly />
-          </label>
-          <label>
-            <input className='transactions' type="text" name='transactionId' value={item.transactionId} onChange={(event) => handleChange(event, index)} />
-          </label>
-        </div>
-      ))}
-      <div className="inputArea">
-        <h2>
-          Output Area
-        </h2>
-        <textarea value={data.map(item => `${item.amount} ${item.phoneNumber} ${item.transactionId}`).join('\n')} readOnly></textarea>
-      </div>
+
+      {
+        data.length > 0 && <>
+          <div className='text-center'>
+            <button onClick={() => setData([])}>
+              Clear
+            </button>
+          </div>
+          <div className='singleData heading'>
+            <label>
+              <input className='amount' type="text" name='amount' value={"Amount"} readOnly />
+            </label>
+            <label>
+              <input className='phoneNumber' type="text" name='phoneNumber' value={"Phone Number"} readOnly />
+            </label>
+            <label>
+              <input className='transactions' type="text" name='transactionId' value={"Transaction Id"} readOnly />
+            </label>
+          </div>
+          {data.map((item, index) => (
+            <div key={index} className='singleData'>
+              <label>
+                <input className='amount' type="text" name='amount' onClick={handleCopyToClipboard} value={item.amount} readOnly />
+              </label>
+              <label>
+                <input className='phoneNumber' type="text" name='phoneNumber' value={item.phoneNumber} onClick={handleCopyToClipboard} readOnly />
+              </label>
+              <label>
+                <input className='transactions' type="text" name='transactionId' value={item.transactionId} onChange={(event) => handleChange(event, index)} />
+              </label>
+            </div>
+          ))}
+          <div className="inputArea">
+            <h2>
+              Output Area
+            </h2>
+            <textarea value={data.map(item => `${item.amount} ${item.phoneNumber} ${item.transactionId}`).join('\n')} readOnly></textarea>
+          </div>
+          <div className='text-center'>
+            <TelegramShareButton url={data.map(item => `${item.amount} ${item.phoneNumber} ${item.transactionId}`).join('\n')}>
+              <button>
+                Share
+              </button>
+            </TelegramShareButton>
+          </div>
+        </>
+      }
     </div>
   );
 };
