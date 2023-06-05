e218cbef8c9e09bb4978022b76190442ebb537e7
riajm <contact.riaj@gmail.com>
2023-06-05 08:42:19 +0600

    sum of numberaaaas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { TelegramShareButton } from 'react-share';
+import { TelegramShareButton } from "react-share";
 
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
 
   const handleChange = (event, index) => {
     const { name, value } = event.target;
-    setData(prevData => {
+    setData((prevData) => {
       const newData = [...prevData];
       newData[index] = { ...newData[index], [name]: value };
       return newData;
@@ -16,77 +16,137 @@ function App() {
   };
 
   const handleDataChange = (event) => {
-    setData(event.target.value.split('\n').map(line => {
-      const [amount, phoneNumber] = line.split(/\s+/);
-      return { amount, phoneNumber, transactionId: '' };
-    }));
+    setData(
+      event.target.value.split("\n").map((line) => {
+        const [amount, phoneNumber] = line.split(/\s+/);
+        return { amount, phoneNumber, transactionId: "" };
+      })
+    );
   };
 
   const handleCopyToClipboard = (event) => {
     event.target.select();
-    document.execCommand('copy');
-    event.target.classList.add('copied');
-
+    document.execCommand("copy");
+    event.target.classList.add("copied");
   };
 
   return (
-    <div className='main'>
+    <div className="main">
       <div className="inputArea">
-        <h2>
-          Input Area
-        </h2>
-        <textarea value={data.map(item => `${item.amount} ${item.phoneNumber}`).join('\n')} onChange={handleDataChange}></textarea>
+        <h2>Input Area</h2>
+        <textarea
+          value={data
+            .map((item) => `${item.amount} ${item.phoneNumber}`)
+            .join("\n")}
+          onChange={handleDataChange}
+        ></textarea>
       </div>
 
-
-      {
-        data.length > 0 && <>
-          <div className='text-center'>
-            <button onClick={() => setData([])}>
-              Clear
-            </button>
+      {data.length > 0 && (
+        <>
+          <div className="text-center">
+            <button onClick={() => setData([])}>Clear</button>
           </div>
-          <div className='singleData heading'>
+          <div className="singleData heading">
             <label>
-              <input className='amount' type="text" name='amount' value={"Amount"} readOnly />
+              <input
+                className="amount"
+                type="text"
+                name="amount"
+                value={"Amount"}
+                readOnly
+              />
             </label>
             <label>
-              <input className='phoneNumber' type="text" name='phoneNumber' value={"Phone Number"} readOnly />
+              <input
+                className="phoneNumber"
+                type="text"
+                name="phoneNumber"
+                value={"Phone Number"}
+                readOnly
+              />
             </label>
             <label>
-              <input className='transactions' type="text" name='transactionId' value={"Transaction Id"} readOnly />
+              <input
+                className="transactions"
+                type="text"
+                name="transactionId"
+                value={"Transaction Id"}
+                readOnly
+              />
             </label>
           </div>
           {data.map((item, index) => (
-            <div key={index} className='singleData'>
+            <div key={index} className="singleData">
               <label>
-                <input className='amount' type="text" name='amount' onClick={handleCopyToClipboard} value={item.amount} readOnly />
+                <input
+                  className="amount"
+                  type="text"
+                  name="amount"
+                  onClick={handleCopyToClipboard}
+                  value={item.amount}
+                  readOnly
+                />
               </label>
               <label>
-                <input className='phoneNumber' type="text" name='phoneNumber' value={item.phoneNumber} onClick={handleCopyToClipboard} readOnly />
+                <input
+                  className="phoneNumber"
+                  type="text"
+                  name="phoneNumber"
+                  value={item.phoneNumber}
+                  onClick={handleCopyToClipboard}
+                  readOnly
+                />
               </label>
               <label>
-                <input className='transactions' type="text" name='transactionId' value={item.transactionId} onChange={(event) => handleChange(event, index)} />
+                <input
+                  className="transactions"
+                  type="text"
+                  name="transactionId"
+                  value={item.transactionId}
+                  onChange={(event) => handleChange(event, index)}
+                />
               </label>
             </div>
           ))}
           <div className="inputArea">
-            <h2>
-              Output Area
-            </h2>
-            <textarea value={data.map(item => `${item.amount} ${item.phoneNumber} ${item.transactionId}`).join('\n')} readOnly></textarea>
+            <h2>Output Area</h2>
+            <textarea
+              value={data
+                .map(
+                  (item) =>
+                    `${item.amount} ${item.phoneNumber} ${item.transactionId}`
+                )
+                .join("\n")}
+              readOnly
+            ></textarea>
+            <p>
+              Total Ammount:{" "}
+              <b> {data.reduce((acc, item) => acc + Number(item.amount), 0)}</b>
+            </p>
           </div>
-          <div className='text-center'>
-            <TelegramShareButton url={data.map(item => `${item.amount} ${item.phoneNumber} ${item.transactionId}`).join('\n')}>
-              <button>
-                Share
-              </button>
+          <div className="text-center">
+            <TelegramShareButton
+              url={data
+                .map(
+                  (item) =>
+                    `${item.amount} ${item.phoneNumber} ${item.transactionId}`
+                )
+                .join("\n")}
+            >
+              <button>Share</button>
             </TelegramShareButton>
           </div>
+          <div className="text-center">
+            Made with ❤️ by{" "}
+            <a href="//riaj.xyz" target="_blank" rel="noreferrer">
+              riaj
+            </a>
+          </div>
         </>
-      }
+      )}
     </div>
   );
-};
+}
 
 export default App;
